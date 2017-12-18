db2bde4a3087ffa30cf3a02dc76027471676a9e6
killersacul <lucas.mouillet@gmail.com>
2017-12-18 13:04:58 +0100

    start of the api with connection to db and logger

---
+++ back/index.php
@@ -0,0 +1,41 @@
+<?php 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+require 'vendor/autoload.php';
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "root";
+$config['db']['pass']   = "root";
+$config['db']['dbname'] = "blogr";
+
+$app = new \Slim\App(["settings" => $config]);
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("logs/blogr.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$container['db'] = function ($c) {
+    // echo "hello world ---------------------------------------------------";
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $this->logger->addInfo("hello " . $name);
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
