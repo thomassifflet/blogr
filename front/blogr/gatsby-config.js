16f2ffcbb274141da1c7963b4dcdd77804d3a980
Thomas Sifflet <thomas.sifflet@gmail.com>
2017-12-19 16:34:07 +0100

    react

--- front/blogr/gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Blogr`
-  },
-  plugins: [`gatsby-plugin-react-helmet`]
-};
--- front/blogr/src/pages/test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Test = () => (
-  <div>
-    <h1>Test coucou</h1>
-  </div>
-);
-
-export default Test;
